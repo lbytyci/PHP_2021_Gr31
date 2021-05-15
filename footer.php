1c9d4f9c3ccb2c6484af957462cff0fd16679807
lbytyci <74509833+lbytyci@users.noreply.github.com>
2021-05-16 00:16:14 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,35 @@
+<?php
+echo '<footer style="margin-top:4%;"><br>
+  <div style="margin-left:9%;margin-top:2%;max-width:100%;">
+     <p><strong>Where to find us:</strong></p>
+    <address><img src="images/location.png" alt="Address" ><span > Kodra e diellit,St.&#199;ajupi 25,10000 Prishtin&#235;,Republika e Kosov&#235;s</span></address><br>
+
+	<p>
+	<img src="images/telephone.png" alt="Telephone"><span class="findfooter"> +386(49)280-103 </span>
+    <img src="images/fax.png" alt="Fax"><span> +377(44)321-645 </span>
+	</p>
+
+	<p>
+    <a href="mailto:info@travel.com?Subject=Hug%20The%20World" style="text-decoration:none;color:black;"><img src="images/email.png" alt="Email"><span class="findfooter"> info@travel.com </span></a>
+       <img src="images/web.png" alt="Web"><span> www.hug-the-world.com </span></p>
+    <!-- Data -->
+    <p id="Clock" style="float:right;margin-top:-12%;margin-right:28%;font-size:17px;"></p>
+	<script src="javascript/clock.js"></script>
+	<!--Lokacioni-->
+     <div style="float:right;margin-top:-5%;margin-right:10%;">
+      <button onclick="getLocation()" class="Location">Your location</button>
+      <p id="loc" ></p>
+    </div>
+  </div>
+
+  <nav style="margin-left:45%;" id="llogo" >
+
+  <a href="https://vimeo.com/search?q=hug%20the%20world" target="_blank">                     <img src="images/vimeo.png" alt="Vimeo" id="imglogo"></a>
+  <a href="https://twitter.com/World" target="_blank">                 <img src="images/twitterlogo.png" alt="Twitter" id="imglogo"></a>
+  <a href="https://www.facebook.com/Hug-The-World-100636051710043" target="_blank">   <img src="images/facebooklogo.png" alt="Facebook" id="imglogo"></a>
+  <a href="https://www.instagram.com/hug__the__world/" target="_blank">        <img src="images/instagramlogo.png" alt="Insta" id="imglogo"></a>
+  <a href="https://www.youtube.com/channel/UCrBPPGyR_7aOd882VpMRkRg" target="_blank">  <img src="images/youtubelogo.png" alt="YT" id="imglogo"></a>
+  <a href="https://www.pinterest.com/explore/family-travel/" target="_blank">   <img src="images/pinterestlogo.png" alt="P" id="imglogo"></a></nav>
+
+</footer>';
+?>
