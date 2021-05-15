d35088a3ef2952de3dba0033fa1c3ba2a8d384b6
sara.haliti@student.uni-pr.edu <sara.haliti@student.uni-pr.edu>
2021-05-16 00:05:27 +0200

    Header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+ echo '<header style="margin-top:-7px;">
+    <div id="hf">
+    <div class="menu">
+      <h3 id="logo">HUG THE WORLD</h3 >
+      <nav>
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="tour.html">Tour</a></li>
+            <li><a href="stats.php">Stats</a></li>
+            <li><a href="kosova.php">Kosova</a></li>
+            <li><a href="aboutus.php">About us</a></li>
+            <li><a href="form1.php">Contact us</a></li>
+        </ul>
+      </nav>
+    </div>
+    </div>
+    
+</header>';
+?>
