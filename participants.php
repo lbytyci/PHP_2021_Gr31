e820572d43a52c421fe99b56f076c6e176aa5894
LiresaFerizaj <74425676+LiresaFerizaj@users.noreply.github.com>
2021-05-21 12:42:41 +0200

    Update participants.php

--- participants.php
+++ participants.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
+$servername = "localhost:3307";
 $user = "root";
 $pass = "root";
 $name = "dbtest";
@@ -24,10 +24,10 @@ th, td {
 }
 </style>';
 echo'<table style="width: 95%; border-collapse: collapse;" >
-<tr style="height: 90px"><th>ID</th><th>First name</th><th>Last name</th><th>Email</th><th>Telephone</th><th>Datat</th><th>Gjinia</th><th>Place</th><th>Level</th><th>gpa</th><th>Course</th><th>Explorer</th></tr>
+<tr style="height: 90px"><th>First name</th><th>Last name</th><th>Email</th><th>Telephone</th><th>Datat</th><th>Gjinia</th><th>Place</th><th>Level</th><th>gpa</th><th>Course</th><th>Explorer</th></tr>
 ';
 while($row = mysqli_fetch_assoc($query)) {
-        echo "<tr style='height: 50px'><td>" . $row["id"]. "</td><td>" . $row["name"]. " </td><td>" . $row["lname"]." </td><td>" . $row["explorer"]." </td><td>" . $row["tel"]." </td><td>" . $row["datat"]." </td><td>" . $row["gjinia"]." </td><td>" . $row["vendi"]." </td><td>" . $row["niveli"]." </td><td>" . $row["gpa"]. " </td><td>" . $row["course"]." </td><td>" . $row["explorer"]."</td></tr>";
+        echo  "</td><td>" . $row["name"]. " </td><td>" . $row["lname"]." </td><td>" . $row["explorer"]." </td><td>" . $row["tel"]." </td><td>" . $row["datat"]." </td><td>" . $row["gjinia"]." </td><td>" . $row["vendi"]." </td><td>" . $row["niveli"]." </td><td>" . $row["gpa"]. " </td><td>" . $row["course"]." </td><td>" . $row["explorer"]."</td></tr>";
     }
 echo"</table>";
 ?>
